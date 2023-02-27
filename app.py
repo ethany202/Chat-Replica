0a13eadff4d1bf0b2b338c95ecf780f838ebf5eb
Ethan Ye <ethan.ye0312@gmail.com>
2023-02-27 00:12:56 -0500

    Yesmen

--- app.py
+++ app.py
@@ -6,38 +6,38 @@ app.config["SECRET_KEY"] = "chat"
 socketio = SocketIO(app, logger=True,
     engineio_logger=True, cors_allowed_origins="*")
 
-available_chats_dict={"yanchovies"}
+# available_chats_dict={"yanchovies"}
 
 @app.route("/", methods=["POST", "GET"])
 def home_page():
     if request.method=='GET':
         #return render_template("home.html")
         return render_template("chat.html", streamer="Yanchovies")
-    else:
-        username = request.form.get("username")
+    # else:
+    #     username = request.form.get("username")
 
-        #conn = rd.connect()
-        #users = rd.check_records(streamer, conn)
+    #     #conn = rd.connect()
+    #     #users = rd.check_records(streamer, conn)
 
-        #rd.close_connection(conn)
+    #     #rd.close_connection(conn)
 
-        #if len(users)>=1:
-        session["chat"]=username
-        return redirect(url_for('direct_to_stream', streamer=username))
+    #     #if len(users)>=1:
+    #     session["chat"]=username
+    #     return redirect(url_for('direct_to_stream', streamer=username))
     
-@app.route("/<streamer>", methods=["POST", "GET"])
-async def direct_to_stream(streamer):
-    if request.method=="GET":
-        available_chats_dict.add(streamer)
-        return render_template("chat.html", streamer=streamer)
-    if request.method=="POST":
-        if streamer in available_chats_dict:
-            message=request.form.get("message")
-            print(message)
+# @app.route("/<streamer>", methods=["POST", "GET"])
+# async def direct_to_stream(streamer):
+#     if request.method=="GET":
+#         available_chats_dict.add(streamer)
+#         return render_template("chat.html", streamer=streamer)
+#     if request.method=="POST":
+#         if streamer in available_chats_dict:
+#             message=request.form.get("message")
+#             print(message)
             
-            # if message=="!end":
-            #     print("ENDING STREAM")
-            #     available_chats_dict.remove(streamer)              
+#             # if message=="!end":
+#             #     print("ENDING STREAM")
+#             #     available_chats_dict.remove(streamer)              
     
 
 @socketio.on('message')
@@ -52,6 +52,6 @@ def handle_message(message):
 
 if __name__=="__main__":
     #app.run(debug=True)
-    socketio.run(app, debug=True, host="mytwitch.herokuapp.com")
+    socketio.run(app)
 # Instead of Python reading from file, JavaScript reads from file and sends random message (POST request) to Python backend
 
