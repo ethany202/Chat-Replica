4c6e21194ad0e59488b6e9c8ad36c8f7c218b69c
Ethan Ye <ethan.ye0312@gmail.com>
2023-02-26 23:40:28 -0500

    Yesmen

--- app.py
+++ app.py
@@ -54,6 +54,6 @@ def handle_message(message):
 
 if __name__=="__main__":
     #app.run(debug=True)
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host="mytwitch.herokuapp.com")
 # Instead of Python reading from file, JavaScript reads from file and sends random message (POST request) to Python backend
 
